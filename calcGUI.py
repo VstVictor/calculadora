950cd48d615e5a0acaf76bcd68db0cd45e07408c
VstVictor <torresvictor.silva@gmail.com>
2023-12-04 14:20:22 -0300

    commit inicial

---
+++ calcGUI.py
@@ -0,0 +1,40 @@
+#interface gráfica
+
+import PySimpleGUI as psg
+
+import calc2
+from calc2 import sub
+
+layout =     [
+                 [psg.Text('Informe o Primeiro Número: '), psg.InputText(key='num1')],
+                 [psg.Text('Informe o Primeiro Número: '), psg.InputText(key='num2')],
+                 [psg.Text('******'), psg.Text('', key='adição'), psg.Text('******')],
+                 [psg.Text('******'), psg.Text('', key='subtração'), psg.Text('******')],
+                 [psg.Text('******'), psg.Text('', key='multiplicação'), psg.Text('******')],
+                 [psg.Text('******'), psg.Text('', key='divisão'), psg.Text('******')],
+                 [psg.Button('calcular'), psg.Button('limpar')],
+             ]
+
+janela = psg.Window('calculadora simples', layout)
+
+while True:
+    evento, valores = janela.read()
+    if evento == psg.WIN_CLOSED:
+        break
+    elif evento == 'limpar':
+        janela['num1'].update('')
+        janela['num2'].update('')
+        janela['adição'].update('')
+        janela['subtração'].update('')
+        janela['multiplicação'].update('')
+        janela['divisão'].update('')
+        janela['num1'].set_focus()
+    else:
+        num1 = float(valores['num1'])
+        num2 = float(valores['num2'])
+        janela ['adição'].update(calc2.soma(num1, num2))
+        janela ['subtração'].update(calc2.sub(num1, num2))
+        janela ['multiplicação'].update(calc2.multi(num1, num2))
+        janela ['divisão'].update(calc2.div(num1, num2))
+
+janela.close()
---
+++ calculadora.py
@@ -0,0 +1,15 @@
+# Calculadora de Soma Simples em Python
+
+# Apresentação
+
+print('\n\t\t\t -- Calculadora de Soma --\n')
+
+# Entrada
+n1 = float(input('Informe N1: '))
+n2 = float(input('Informe N2: '))
+
+# Processamento
+total = n1/n2
+
+#Saída
+print(f'{n1} / {n2} = {total}')
