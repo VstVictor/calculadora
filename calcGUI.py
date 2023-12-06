06e24ef546e0b556df251bf4ed6fd3b8325fdfb7
VstVictor <torresvictor.silva@gmail.com>
2023-12-06 15:57:49 -0300

    commit inicial

--- calcGUI.py
+++ calcGUI.py
@@ -2,7 +2,7 @@ import PySimpleGUI as psg
 from calc2 import soma, sub, multi, div
 
 layout = [
-    [psg.Text('Escolha a operação:')],
+    [psg.Text('Escolha a Operação:')],
     [psg.Radio('Adição', 'operacao', key='adição'), psg.Radio('Subtração', 'operacao', key='subtracao')],
     [psg.Radio('Multiplicação', 'operacao', key='multiplicacao'), psg.Radio('Divisão', 'operacao', key='divisao')],
     [psg.Text('Informe o Primeiro Número: '), psg.InputText(key='num1')],
@@ -23,8 +23,20 @@ while True:
             janela[key].update('')
         janela['num1'].set_focus()
     elif evento == 'Calcular':
-        num1 = float(valores['num1'])
-        num2 = float(valores['num2'])
+        num1_str = valores['num1']
+        num2_str = valores['num2']
+
+        if not num1_str or not num2_str:
+            psg.popup_error("Informe valores para os dois números.")
+            continue
+
+        try:
+            num1 = float(num1_str)
+            num2 = float(num2_str)
+        except ValueError:
+            psg.popup_error("Os valores informados não são números válidos.")
+            continue
+
         operacao = None
 
         for op in ['adição', 'subtracao', 'multiplicacao', 'divisao']:
@@ -35,19 +47,19 @@ while True:
         if operacao is not None:
             if operacao == 'adição':
                 resultado = soma(num1, num2)
-                [janela['resultado'].update(f'{num1} + {num2} = {resultado}')]
+                janela['resultado'].update(f'{num1} + {num2} = {resultado}')
             elif operacao == 'subtracao':
                 resultado = sub(num1, num2)
-                [janela['resultado'].update(f'{num1} - {num2} = {resultado}')]
+                janela['resultado'].update(f'{num1} - {num2} = {resultado}')
             elif operacao == 'multiplicacao':
                 resultado = multi(num1, num2)
-                [janela['resultado'].update(f'{num1} * {num2} = {resultado}')]
+                janela['resultado'].update(f'{num1} * {num2} = {resultado}')
             elif operacao == 'divisao':
                 if num2 != 0:
                     resultado = div(num1, num2)
                     janela['resultado'].update(f'{num1} / {num2} = {resultado}')
                 else:
-                    janela['resultado'].update(f'{num1} / {num2} = 0')
+                    janela['resultado'].update('Erro: Divisão por zero!')
         else:
             psg.popup_error("Escolha uma operação antes de calcular!")
 
